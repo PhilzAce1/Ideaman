f7dc9ddd25aac5dba560e1a0edc1bb8cb8b2e6dc
Philzace1 <unknownumeh@gmail.com>
2020-08-14 23:50:38 +0100

    first commit

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,2 +1,36 @@
 import '../sass/main.scss';
-console.log('Webpack Boilerplate');
+document.body.onload = () => {
+  loadingEffect();
+};
+function loadingEffect() {
+  const dv = gsap.timeline();
+  dv.to('.loader_container', {
+    opacity: 0,
+    duration: 1,
+  })
+    .to('.loader_container', {
+      display: 'none',
+    })
+    .to('.welcome_conatainer div', {
+      duration: 0.3,
+      text: {
+        value: 'IDEAMAN',
+        delimiter: '',
+      },
+    })
+    .to('.welcome_conatainer', {
+      opacity: 0,
+      duration: 1,
+      delay: 1,
+    })
+    .to('.welcome_conatainer', {
+      display: 'none',
+    })
+    .from('.text article', {
+      x: -200,
+      opacity: 0,
+    })
+    .from('.low button', {
+      scale: 0,
+    });
+}
